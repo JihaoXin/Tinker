1282887f94b9c29b4a23cc51744e1d31dcdd66ca
ALBINALSHAIKH <sadiqg8@gmail.com>
2022-12-11 13:43:23 +0000

    Fixed decoding negative literals

--- devices/instruction_decode.cpp
+++ devices/instruction_decode.cpp
@@ -21,11 +21,7 @@ void InstructionDecode::receive_clock() {
 
     /* --- Instruction Decode --- */
     
-    //Decode the instruction to the opcode, r_d, r_s, r_t, and literal
-    // if (*inport[0] == std::numeric_limits<long long>::min()) { // end of file reached.
-    //     opcode = std::numeric_limits<long long>::min();
-    //     return;
-    // }
+
     opcode = (*inport[0] >> 27) & 0b11111;
     if (opcode == 31) // so far, we still need the below registers to maintain their last values for execution to finish.
         return;
@@ -33,4 +29,12 @@ void InstructionDecode::receive_clock() {
     register_s = (*inport[0] >> 17) & 0b11111;
     register_t = (*inport[0] >> 12) & 0b11111;
     literal = *inport[0] & 0b111111111111;
+
+    //Handle negtives by testing the sign bit
+    if (literal & 0b100000000000) {
+        //Literal is negative, pad zeroes in the most significant 52 bits
+        
+        literal = (signed long long) 0xFFFFFFFFFFFFF000 ^ literal;
+    }
+    
 }
